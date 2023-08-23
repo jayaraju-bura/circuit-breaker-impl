97042d63e300e31a96763d6a34957d34cf463c1d
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2023-08-24 00:44:51 +0530

    doc fix

--- server.py
+++ server.py
@@ -6,30 +6,30 @@ app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/success')
-def success_endpoint():
+@app.route('/positive')
+def postive_test_endpoint():
     return {
-        "msg": "Call to this endpoint /success has succeeded"
+        "msg": "Call to this endpoint /positive has succeeded"
     }, 200
 
 
-@app.route('/failure')
-def faulty_endpoint():
+@app.route('/negative')
+def negative_test_endpoint():
     r = random.randint(0, 1)
     if r == 0:
         time.sleep(2)
 
     return {
-        "msg": "call to the endpoint /failure has not succeeded"
+        "msg": "call to the endpoint /negative has not succeeded"
     }, 500
 
 
-@app.route('/random')
+@app.route('/arbitrary')
 def fail_randomly_endpoint():
     r = random.randint(0, 1)
     if r == 0:
         return {
-            "msg": "Success msg"
+            "msg": "call to API endpopint arbitray method has succeeded"
         }, 200
 
     return {
